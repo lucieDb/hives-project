2d0aebe22e23a12d3cc2f033ce5aa889cb7731f7
Lucie <debrabanderelucie@gmail.com>
2024-05-31 11:24:06 +0200

    add validation hive model

--- app/models/hive.rb
+++ app/models/hive.rb
@@ -1,2 +1,9 @@
 class Hive < ApplicationRecord
+  validates :name,
+            length: { minimum: 6,
+                      too_short: '%{value} is not allowed, please add a new name with at least 6 characters' },
+            presence: true
+
+  validates :weight,
+            presence: true, numericality: { only_integer: true }
 end
