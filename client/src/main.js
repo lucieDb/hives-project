1526f3cba38c03d148e496557348622ccbed3d89
Lucie <debrabanderelucie@gmail.com>
2024-05-31 13:05:23 +0200

    import router

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import Router from './router/router.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
-createApp(App).mount('#app')
+createApp(App).use(Router).mount('#app')
