18006ec6ca92c59e9448b552b9f8fae7f1b6c71e
Lucie <debrabanderelucie@gmail.com>
2024-05-31 12:28:09 +0200

    create hive mechanical

--- client/src/components/Hives/Hives.vue
+++ client/src/components/Hives/Hives.vue
@@ -6,13 +6,16 @@
       <div class='row'>
         <div class='col-6'>
           <h2>create a hive</h2>
-          
+          <br>
+          <input type="text" v-model="name" placeholder="enter the new hive name" />
+          <input type="text" v-model="weight" placeholder="enter the new hive weight" />
+          <button @click="createHive" type="button" class="btn btn-primary">Create</button>
         </div>
         <div class='col-6'>
           <h2>hives list</h2>
+          <br>
           <li v-for="hive in hives" :key="hive.id">
-            {{ hive.name }}
-            {{ hive.weight }}
+            {{ hive.name }} for {{ hive.weight }} kg
           </li>
         </div>
       </div>
@@ -24,11 +27,32 @@
 import { ref, onMounted } from 'vue'
 
 const hives = ref([])
-const API_URL = "http://localhost:3000/api/v1/hives"
+const name = ref('')
+const weight = ref('')
+const API_URL = "http://localhost:3000/api/v1/hives";
 
 onMounted(async () => {
     const res = await fetch(API_URL)
     hives.value = await res.json()
 })
+
+const createHive = async () => {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name.value,
+      weight: weight.value
+    })
+  })
+
+  const data = await response.json();
+
+  hives.value.push(data)
+  name.value = ''
+  weight.value = ''
+}
 
 </script>
