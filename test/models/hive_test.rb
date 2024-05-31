d59822e84a1ecd76b06c98a6aaa25f3acfd503ff
Lucie <debrabanderelucie@gmail.com>
2024-05-31 11:24:28 +0200

    add test hive model

--- test/models/hive_test.rb
+++ test/models/hive_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class HiveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @hive = Hive.new(name: 'BeesBees', weight: 9)
+  end
+
+  test 'valid hive' do
+    assert @hive.valid?
+  end
+
+  test 'invalid with short name' do
+    @hive.name = 'C'
+    refute @hive.valid?
+    assert @hive.errors[:name]
+  end
+
+  test 'invalid with another string type for name' do
+    @hive.name = 12
+    refute @hive.valid?
+    assert @hive.errors[:name]
+  end
+
+  test 'invalid without name' do
+    hive_no_name = Hive.new(weight: 15)
+    refute hive_no_name.valid?
+    assert hive_no_name.errors[:name]
+  end
+
+  test 'invalid with another integer type for weight' do
+    @hive.weight = 'Yes'
+    refute @hive.valid?
+    assert @hive.errors[:weight]
+  end
+
+  test 'invalid without weight' do
+    hive_no_weight = Hive.new(name: 'Beel Murray')
+    refute hive_no_weight.valid?
+    assert hive_no_weight.errors[:weight]
+  end
 end
